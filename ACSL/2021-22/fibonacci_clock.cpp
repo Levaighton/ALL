3f571e650226061c53a6125db79ea2bfc33804e1
Leighton Chen <chenletong19@gmail.com>
2023-11-20 15:21:09 +0800

    finish question fibonacci clock

--- ACSL/2021-22/fibonacci_clock.cpp
+++ ACSL/2021-22/fibonacci_clock.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main(){
+    for (int n = 0 ; n < 5 ; ++n ){
+        char lo, uo, two, three, five ;
+        cin >> lo >> uo >> two >> three >> five ;
+        //Red
+        int hour = 0, min = 0 ;
+        if(lo == 'R') hour += 1;
+		if(uo == 'R') hour += 1;
+		if(two == 'R') hour += 2;
+		if(three == 'R') hour += 3;
+		if(five == 'R') hour += 5;
+        // green
+		if(lo == 'G') min += 1;
+		if(uo == 'G') min += 1;
+		if(two == 'G') min += 2;
+		if(three == 'G') min += 3;
+		if(five == 'G') min += 5;
+        if(lo == 'B') hour += 1, min += 1;
+		if(uo == 'B') hour += 1, min += 1;
+		if(two == 'B') hour += 2, min += 2;
+		if(three == 'B') hour += 3, min += 3;
+		if(five == 'B') hour += 5, min += 5;
+		min *= 5;
+		if(min >= 60) hour += (min/60), min %= 60;
+		if(hour > 11) hour %= 12;
+		cout<< setw(2) << setfill('0') << hour << ':'
+			<< setw(2) << setfill('0') << min << endl;
+    }
     return 0;
 }
